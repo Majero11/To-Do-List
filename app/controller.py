812e809ae1f508228a6a2441501217bb90912a92
Tinmaj <majerographics@gmail.com>
2026-03-07 15:57:15 +0000

    create index functionality. handle the form request

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,7 @@
 from flask import render_template, redirect, url_for, request, session, flash
 
 from config.database import get_db
+from models import DatabaseOperations
 
 from app import app
 
@@ -17,18 +18,28 @@ def index():
         if the user is a employee render the employee dashboard
     """
     if request.method == "POST":
-        username = request.form.get("user") # joseiungo
+        username_email = request.form.get("user") # joseiungo
         password = request.form.get("password")
 #jose@iungo.
-        if username.find('@') != -1: # check if joseiungo
+        if username_email.find('@') != -1: # check if joseiungo
             # invoke a function to retrieve the user from the database ussing the email
+            user = DatabaseOperations.get_user_by_username(username_email, password)
         else:
             # invoke a function to retrieve the user from the database using the username
-        
+            user = DatabaseOperations.get_user_by_username(username_email, password)
         # check if the user retrieve from the database  != None -> create the session
-
+        if user is not None:
             # redirect by role
+            session["user_id"] = user[0]
+            session["user_name"] = user[1]
+            session["is_admin"] = user[2]
+            
+            #redirect by role
             # if the user is admin
+            if session["is_admin"]:
+                return redirect("/admin_dashboard")
+            
+            return redirect("/user_dashboard")
 
         #if the user doesn't exist:
         #display a message
